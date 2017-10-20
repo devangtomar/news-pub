413ff618c58ec28c89c8240d4f487cdfa2a3ae38
d0t077o <devangtomar123@gmail.com>
2017-10-20 19:42:54 +0530

    prepend  the file metas.php

---
+++ metas.php
@@ -0,0 +1,3 @@
+<meta name="keywords" content="king, flames, bahamex, ACASS, Andron's, Computing, Solution, Software, Studios, Programs, websites" />
+<meta name="content-type" content="text/html" />
+<meta name="description" content="King Flames Online Community" />
